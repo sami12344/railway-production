d536ab4801ea36742a14b7e334617f61a30d91ad
sami12344 <freelancersami16203@gmail.com>
2023-07-18 21:40:57 +0600

    dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import express, { Application, Request, Response } from 'express'
+import dotenv from 'dotenv'
+dotenv.config()
 
 const app: Application = express()
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello world ts')
 })
-
-app.listen(5000, () => {
-  console.log('Server running at http://localhost:5000')
+const port = process.env.PORT
+app.listen(port, () => {
+  console.log('Server running at http://localhost:'+port)
 })
